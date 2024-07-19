42db077669aaf3ca881705cdd988a91a9a33df81
buğra uysal <uysalbugra134@gmail.com>
2024-07-19 04:28:22 +0300

    sıfırla buttonu eklendi,  arttırılan  sayıyı sıfırlar

--- zkri.py
+++ zkri.py
@@ -3,16 +3,18 @@ import tkinter as tk
 
 
 sayfa=tk.Tk()
-""" zikir sayısı artışı """
-
+""" zikir sayısı başlangıcı sayacı  """
 zikir_sys=0
-
+""" zikir sayısını arttırmak """
 def artı():
     global zikir_sys
     zikir_sys +=1
     adet.config(text=str(zikir_sys))
 
-
+""" zikir sayısını sıfırlama """
+def zro():
+     zikir_sys=0
+     adet.config(text=zikir_sys)
 
 
 
@@ -21,10 +23,12 @@ def artı():
 sayfa.geometry("600x600")
 """ ne kadar zikir çekdiğin """
 adet=tk.Label(sayfa, text=str(zikir_sys) )
-adet.place(x=250, y=150, height=25,width=50)
+adet.place(x=265, y=150, height=25,width=50)
 """ bas buttonu sayıyı arttırr button """
 btn=tk.Button(sayfa,text="bas",command=artı)
-btn.place(y=300,x=250,height=25,width=80)
+btn.place(y=210,x=250,height=25,width=80)
 
- 
+""" zikir_sys yi sıfırlamak  """
+sfr=tk.Button(sayfa,text="sıfırla", command=zro)
+sfr.place(height=25, width=80,x=250,y=270)
 sayfa.mainloop()
