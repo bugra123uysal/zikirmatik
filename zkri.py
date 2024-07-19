0d79324dfc15cd0b6f60664be05d663f2518cfbd
buğra uysal <uysalbugra134@gmail.com>
2024-07-20 02:25:06 +0300

    zikir çekilecek kelime yazılır ve buttona basarak label e yazdırılır

--- zkri.py
+++ zkri.py
@@ -16,8 +16,17 @@ def zro():
      zikir_sys=0
      adet.config(text=zikir_sys)
 
+""" yazılan zikiri ekranda gösterme """
 
+def gstr():
+     """ yazı al """
+    
+     gt=yazz.get()
 
+     wiv.config(text=gt)
+  
+
+     
 
 
 sayfa.geometry("600x600")
@@ -28,7 +37,21 @@ adet.place(x=265, y=150, height=25,width=50)
 btn=tk.Button(sayfa,text="bas",command=artı)
 btn.place(y=210,x=250,height=25,width=80)
 
+
 """ zikir_sys yi sıfırlamak  """
 sfr=tk.Button(sayfa,text="sıfırla", command=zro)
 sfr.place(height=25, width=80,x=250,y=270)
+
+
+""" ekranda gözükücek zikiri yazma yeri  """
+yazz=tk.Entry(sayfa,)
+yazz.place(x=170 , y=30, height=20, width=220)
+
+""" çekilcek zikiri ekranda gözükecek yer """
+
+wiv=tk.Label(sayfa, text="",wraplength=250,justify="left")
+wiv.place(x=170 , y=85, height=50, width=250)
+""" kaydet buttonu zikiri kadeder """
+kyd=tk.Button(sayfa,text="kaydet", command=gstr)
+kyd.place(x=400, y=30, height=20 , width=80)
 sayfa.mainloop()
